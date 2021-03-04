f8b1071538e31117794f4ee3028cdf1fd29c5556
unibon4141 <vhcdyufg@gmail.com>
2021-03-04 16:37:19 +0900

    トップページを追加

---
+++ index.php
@@ -0,0 +1,35 @@
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>トップページ</title>
+</head>
+<body>
+  <header>
+    <div class="logo">
+      <img src="image/logo.png" alt="ロゴ">
+    </div>
+    <nav>
+      <ul>
+        <li><a href="#">放送中のアニメ</a></li>
+        <li><a href="#">アニメを検索</a></li>
+        <li><a href="#">マイページ</a></li>
+      </ul>
+    </nav>
+    <div class="start-button">
+      <a href="#">ログイン</a>
+      <a href="#">ユーザー登録</a>
+    </div>
+  </header>
+  <main id="main">
+  
+  </main>
+  <footer>
+  Copyright アニメログ. All Rights Reserved.
+  </footer>
+</body>
+</html>
