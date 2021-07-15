50f32852a50c294bb48347ceb4072b9e51e45a51
unibon4141 <vhcdyufg@gmail.com>
2021-07-15 23:54:45 +0900

    commit

--- function.php
+++ function.php
@@ -9,33 +9,35 @@ ini_set('error_log', './log/php.log');
 
 
 
-ini_set('session.gc_maxlifetime', 60*60*24*30);
-ini_set('session.cookie_lifetime ', 60*60*24*30);
+ini_set('session.gc_maxlifetime', 60 * 60 * 24 * 30);
+ini_set('session.cookie_lifetime ', 60 * 60 * 24 * 30);
 
- // セッション開始
+// セッション開始
 session_start();
 session_regenerate_id();
 
 $errMsg = array();
 
-  define('MSG01','入力必須です。');
-  define('MSG02','メールアドレスを正しく入力してください。');
-  define('MSG03','パスワードは6文字以上12文字以内で入力してください。');
-  define('MSG04', '既に使用されているメールアドレスです。');
-  define('MSG05', 'メールアドレスまたはパスワードが間違っています。');
+define('MSG01', '入力必須です。');
+define('MSG02', 'メールアドレスを正しく入力してください。');
+define('MSG03', 'パスワードは6文字以上12文字以内で入力してください。');
+define('MSG04', '既に使用されているメールアドレスです。');
+define('MSG05', 'メールアドレスまたはパスワードが間違っています。');
 
-  //ログの出力変更用フラグ
-$debugFlg = 0;
+//ログの出力変更用フラグ
+$debugFlg = 1;
 // ログを出力
-function debug($str) {
+function debug($str)
+{
   global $debugFlg;
-  if($debugFlg){
-  error_log($str);
+  if ($debugFlg) {
+    error_log($str);
   }
 }
 
 // DBに接続
-function dbConnect(){
+function dbConnect()
+{
   $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
   $db['dbname'] = ltrim($db['path'], '/');
   $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
@@ -53,25 +55,27 @@ function dbConnect(){
   return $dbh;
 }
 
-function queryPost($dbh, $sql, $data ){
+function queryPost($dbh, $sql, $data)
+{
   $stmt = $dbh->prepare($sql);
-  foreach($data as $d){
+  foreach ($data as $d) {
     $stmt->bindValue($d[0], $d[1], $d[2]);
   }
   $result = $stmt->execute();
-  if($result){
+  if ($result) {
     debug('SQLは成功しました。');
   } else {
     debug('SQLは失敗しました。');
-    debug('SQLエラー：'.$stmt->errorInfo());
+    debug('SQLエラー：' . $stmt->errorInfo());
   }
   return $stmt;
 }
 
 // メールアドレス重複チェック
-function emailValidDup($email){
+function emailValidDup($email)
+{
   global $errMsg;
-  try{
+  try {
     $dbh = dbConnect();
     $sql = 'SELECT count(*) FROM users WHERE email = :email AND delete_flg = 0';
     $data = array(
@@ -83,50 +87,53 @@ function emailValidDup($email){
     debug(print_r($result, true));
 
     // 重複したメールアドレスがDBに存在した場合
-    if($result['count(*)']> 0) {
+    if ($result['count(*)'] > 0) {
       $errMsg['email'] = MSG04;
-    } 
-  } catch(PDOException $e){
-    error_log('エラー発生：'.$e->getMessage());
+    }
+  } catch (PDOException $e) {
+    error_log('エラー発生：' . $e->getMessage());
   }
 }
 
 // 未入力チェック
-function requireValid($str, $name){
+function requireValid($str, $name)
+{
   global $errMsg;
-  if(empty($str)){
+  if (empty($str)) {
     $errMsg[$name] = MSG01;
   }
 }
 
 // メールアドレス形式チェック
-function emailFormatValid($email, $name){
+function emailFormatValid($email, $name)
+{
   global $errMsg;
-  if(!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\?\*\[|\]%'=~^\{\}\/\+!#&\$\._-])*@([a-zA-Z0-9_-])+\.([a-zA-Z0-9\._-]+)+$/",$email)){
+  if (!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\?\*\[|\]%'=~^\{\}\/\+!#&\$\._-])*@([a-zA-Z0-9_-])+\.([a-zA-Z0-9\._-]+)+$/", $email)) {
     $errMsg[$name] = MSG02;
   }
 }
 
 // 文字数チェック
-function strLenValid($str, $name, $min = 6, $max = 12){
+function strLenValid($str, $name, $min = 6, $max = 12)
+{
   global $errMsg;
-if(mb_strlen($str) < $min  || mb_strlen($str) > $max){
-  $errMsg[$name]  = MSG03;
+  if (mb_strlen($str) < $min  || mb_strlen($str) > $max) {
+    $errMsg[$name]  = MSG03;
   }
 }
 
-function isLogin() {
-  if(!empty($_SESSION['login_time'])){
+function isLogin()
+{
+  if (!empty($_SESSION['login_time'])) {
     debug('ログイン済みのユーザーです');
     // ログイン有効期限（３０日）を超えていた場合
-    if($_SESSION['login_time'] +60*60*24*30 < time()) {
+    if ($_SESSION['login_time'] + 60 * 60 * 24 * 30 < time()) {
       $isLogin = 0;
-  
     } else {
       debug('ログイン有効期限内です');
       // ログイン日時を更新
-     $isLogin = 1;
-      }
+      $isLogin = 1;
+    }
   } else {
     debug('未ログインユーザーです');
     $isLogin = 0;
@@ -135,8 +142,7 @@ function isLogin() {
 }
 
 // サニタイズ
-function sanitize($str){
+function sanitize($str)
+{
   return htmlspecialchars($str, ENT_QUOTES);
 }
-
-?>
--- search.php
+++ search.php
@@ -7,35 +7,40 @@ debug('アニメ検索ページ');
 $current_year = date('Y');
 $oldest_year = 2014;
 
-if(!empty($_POST)){
+if (!empty($_POST)) {
   debug('POST送信があります');
-  debug('POST内容：'.print_r($_POST, true));
+  debug('POST内容：' . print_r($_POST, true));
   $api = 'http://api.moemoe.tokyo/anime/v1/master';
   $year = $_POST['year'];
   $season = $_POST['season'];
   $season_word = '';
-  if(!empty($season)){
-    switch($season){
+  if (!empty($season)) {
+    switch ($season) {
       case 2:
         $season_word = '春';
+        break;
       case 3:
         $season_word  = '夏';
+        break;
       case 4:
         $season_word = '秋';
+        break;
       case 1:
         $season_word = '冬';
+        break;
     }
   }
   $response = file_get_contents("$api/$year/$season");
 
   $animeList = json_decode($response);
 
-  debug('アニメ検索結果：'.print_r($animeList, true));
+  debug('アニメ検索結果：' . print_r($animeList, true));
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -49,51 +54,51 @@ if(!empty($_POST)){
   <title>アニメ検索</title>
 </head>
 <?php
-  include('header.php');
-  ?>
-  <main id="main">
-    <div class="container">
-  <h1>アニメを探す</h1>
-  <form action="" method="post">
-    <select name="year">
-      <?php
-        for($i = $oldest_year; $i <= $current_year;$i++){
-          ?>
-          <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-          <?php
+include('header.php');
+?>
+<main id="main">
+  <div class="container">
+    <h1>アニメを探す</h1>
+    <form action="" method="post">
+      <select name="year">
+        <?php
+        for ($i = $oldest_year; $i <= $current_year; $i++) {
+        ?>
+          <option value="<?php echo $i; ?>" <?php if (!empty($year) && $year == $i) echo "selected" ?>><?php echo $i; ?></option>
+        <?php
         }
-      ?>
-    </select>
-    <select name="season" id="">
-        <option value="2">春アニメ</option>
-        <option value="3">夏アニメ</option>
-        <option value="4">秋アニメ</option>
-        <option value="1">冬アニメ</option>
-    </select>
-    <input type="submit" value="検索">
-  </form>
-  <?php 
-  if(!empty($animeList)){
-    ?>
-    <h2>検索結果 : <?php if(!empty($year) && !empty($season)) echo $year.'年 '.$season_word.'アニメ'; ?></h2>
-    <ul class="anime-list">
+        ?>
+      </select>
+      <select name="season" id="">
+        <option value="2" <?php if (!empty($season) && $season == 2) echo "selected" ?>>春アニメ</option>
+        <option value="3" <?php if (!empty($season) && $season == 3) echo "selected" ?>>夏アニメ</option>
+        <option value="4" <?php if (!empty($season) && $season == 4) echo "selected" ?>>秋アニメ</option>
+        <option value="1" <?php if (!empty($season) && $season == 1) echo "selected" ?>>冬アニメ</option>
+      </select>
+      <input type="submit" value="検索">
+    </form>
     <?php
-    foreach($animeList as $anime){
-      ?>
-      <li class="anime">
-        <p class="anime-title"><?php echo $anime->title; ?></p>
-        <button class="add-button <?php if(isLogin()) echo 'myanime-add'; ?>" data-title = "<?php echo sanitize($anime->title); ?>">
-マイアニメに登録する</button>
-      </li>
-      <?php
+    if (!empty($animeList)) {
+    ?>
+      <h2>検索結果 : <?php if (!empty($year) && !empty($season)) echo $year . '年 ' . $season_word . 'アニメ'; ?></h2>
+      <ul class="anime-list">
+        <?php
+        foreach ($animeList as $anime) {
+        ?>
+          <li class="anime">
+            <p class="anime-title"><?php echo $anime->title; ?></p>
+            <button class="add-button <?php if (isLogin()) echo 'myanime-add'; ?>" data-title="<?php echo sanitize($anime->title); ?>">
+              マイアニメに登録する</button>
+          </li>
+        <?php
+        }
+        ?>
+      </ul>
+    <?php
     }
     ?>
-    </ul>
-    <?php
-  }
-  ?>
   </div>
-  </main>
+</main>
 <?php
-  include('footer.php');
-  ?>
+include('footer.php');
+?>
